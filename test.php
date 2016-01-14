559a059016e8f91b804db5dcb726afb03eadedaa
Victor <bocharsky.bw@gmail.com>
2016-01-14 18:24:04 +0200

    Data types in PHP

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+$text = 'text'; // string
+$a = 5.7; // float
+$b = 7; // integer
+$bool = true; // boolean
+// array
+// object
+// resource
+
+$c = $a * $b;
+
+$int = (int)'123.45';
+// echo $int;
+
+var_dump($text);
