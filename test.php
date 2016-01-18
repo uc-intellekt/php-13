c0ff4064817516093871a8e10dcf4a052f0e1334
Victor <bocharsky.bw@gmail.com>
2016-01-18 18:36:21 +0200

    Work with arrays and cycles

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $text = 'text'; // string
 $a = 5.7; // float
 $b = 7; // integer
 $bool = true; // boolean
-// array
+$arr = [1,2,3]; // array
 // object
 // resource
 
@@ -13,4 +13,45 @@ $c = $a * $b;
 $int = (int)'123.45';
 // echo $int;
 
-var_dump($text);
+// var_dump($text);
+
+$arr = [
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+];
+
+// var_dump($arr);
+// echo $arr['two'];
+
+$serverLangs = [
+    'php',
+    'python',
+    'ruby',
+];
+$clientLangs = [
+    'JavaScript',
+];
+
+$langs = [
+    'server' => $serverLangs,
+    'client' => $clientLangs,
+];
+// var_dump($langs);
+
+$countLangs = count($langs['server']);
+for ($i = 0; $i < $countLangs; $i++) { // $i++ === $i = $i + 1
+    // echo $langs['server'][$i];
+}
+
+echo '<ul class="list">'."\n";
+foreach ($langs as $groupName => $langGroup) {
+    foreach ($langGroup as $lang) {
+        // echo '<li>';
+        // echo $lang;
+        // echo '</li>';
+        // echo '<li>' . $lang . '</li>';
+        echo "\t<li>{$lang}</li>\n";
+    }
+}
+echo '</ul>';
