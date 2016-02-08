0e88e463ccecbd4a7c51f71fca8e0ce0ae1139ea
bocharsky-bw <bocharsky.bw@gmail.com>
2016-02-08 17:34:54 +0200

    Add Post controller with a few actions

---
+++ site/src/Controller/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Controller;
+
+use Doctrine\DBAL\Connection as Conn;
+use Silex\Application as App;
+
+class PostController
+{
+    public function indexAction(App $app)
+    {
+        /** @var Conn $db */
+        $db = $app['db'];
+
+        $posts = $db->fetchAll('SELECT * FROM post');
+        var_dump($posts); die;
+    }
+
+    public function showAction(App $app, $id)
+    {
+        /** @var Conn $db */
+        $db = $app['db'];
+
+        $post = $db->fetchAssoc("SELECT *
+            FROM post
+            WHERE id = $id
+            LIMIT 1
+        ");
+        var_dump($post); die;
+    }
+}
--- site/web/index.php
+++ site/web/index.php
@@ -1,10 +1,23 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
+//require_once __DIR__.'/../src/Controller/PostController.php';
 
 use Silex\Application as App;
 
 $app = new App();
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver' => 'pdo_mysql',
+        'dbname' => 'php_15',
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => 'usbw',
+        'port' => 3307,
+    ),
+));
 
 $app->get('/', function() use($app) {
     return 'Welcome!';
@@ -12,5 +25,7 @@ $app->get('/', function() use($app) {
 $app->get('/hello/{userName}', function($userName) use($app) {
     return 'Hello '.$app->escape($userName);
 });
+$app->get('/blog', 'Controller\\PostController::indexAction');
+$app->get('/blog/{id}', 'Controller\\PostController::showAction');
 
 $app->run();
